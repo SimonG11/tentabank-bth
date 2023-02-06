ecf3a5b4254848bf54d9a9216f304b92624c1d5f
Simon G <103114747+SimonG11@users.noreply.github.com>
2023-02-06 11:24:07 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import Navbar from './components/index';
+import { BrowserRouter as Router, Routes, Route}
+	from 'react-router-dom';
+import Home from './pages/index';
+import About from './pages/about';
+import Browse from './pages/browse';
+import Profile from './pages/profile';
+import Upload from './pages/upload';
+import Footer from './components/footer'
+
+function App() {
+
+return (
+	<Router>
+	<Navbar />
+	<Routes>
+		<Route exact path='/' element={<Home />} />
+		<Route path='/about' element={<About/>} />
+		<Route path='/upload' element={<Upload/>} />
+		<Route path='/browse' element={<Browse/>} />
+		<Route path='/profile' element={<Profile/>} />
+	</Routes>
+	<Footer />
+	</Router>
+);
+}
+
+export default App;
